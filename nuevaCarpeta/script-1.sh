a75856d61b707130ad88b21b5c1721ea16a200f4
herviatl <311874013+herviatl@users.noreply.github.com>
2026-08-14 10:38:31 -0400

    primer script

---
+++ nuevaCarpeta/script-1.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+echo "hola soy un script"
+
+read -p "Inreoduce el primer número: "  num1
+read -p "Introduce el sefundo número: " num2
+
+suma=$((num1 + num2))
+
+echo "la suma es: $suma"
+
+
+
+
